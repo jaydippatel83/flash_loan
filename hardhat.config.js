9974e54e26ca0b68f5daeb1f25a0342bf8e3fbcb
Jaydippatel <macbook@MacBooks-MacBook-Pro-2.local>
2022-12-08 15:47:06 +0530

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+ 
+const MUMBAI_RPC_URL = `https://polygon-mainnet.g.alchemy.com/v2/${process.env.MUMBAI_RPC_URL}`; 
+// console.log(MUMBAI_RPC_URL);
+
+// const MUMBAI_RPC_URL = `https://polygon-mumbai.g.alchemy.com/v2/${process.env.MUMBAI_RPC_URL}` ;
+// const PRIVATE_KEY = process.env.PRIVATE_KEY;
+// const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
+
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+    mumbai: { 
+      url: MUMBAI_RPC_URL,
+      accounts: [PRIVATE_KEY], 
+    },  
+  }, 
+  etherscan: {
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_KEY,
+    },
+  }, 
+};
